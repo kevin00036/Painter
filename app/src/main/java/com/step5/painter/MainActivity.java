4c43cb9fb9cd744e7c6e6cc483c8cb9d077a804c
YourName <YourEmail>
2015-03-10 17:10:38 +0800

    Add line

--- app/src/main/java/com/step5/painter/MainActivity.java
+++ app/src/main/java/com/step5/painter/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,16 +16,21 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Random;
 
 
 public class MainActivity extends Activity {
+
+    myView mv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("Debug", "CREATTTTTTTE");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mv = (myView) findViewById(R.id.canvasView);
         //setContentView(new myView(this));
     }
 
@@ -52,18 +59,48 @@ public class MainActivity extends Activity {
 
     public void buttonClicked(View view)
     {
-        final myView mv = (myView) findViewById(R.id.canvasView);
         mv.drawCircle();
+    }
+
+    public void undoClicked(View view)
+    {
+        mv.undo();
+    }
+
+    public void selectPencil(View view)
+    {
+        mv.setMode(mv.MODE_PENCIL);
+    }
+    public void selectLine(View view)
+    {
+        mv.setMode(mv.MODE_LINE);
+    }
+    public void selectColor(View view)
+    {
+
     }
 }
 
 class myView extends View
 {
+    final int MODE_PENCIL = 1;
+    final int MODE_LINE = 2;
+
+    final int MAX_UNDO = 10;
+
     Bitmap currentBitmap;
+    Bitmap bufferBitmap;
+    Deque<Bitmap> undoQueue;
+
     Canvas currentCanvas;
     int Width;
     int Height;
     boolean resized = false;
+    int mode = MODE_PENCIL;
+
+    float lastX, lastY;
+    Path pencilPath;
+
 
     public void init()
     {
@@ -71,9 +108,11 @@ class myView extends View
         measure(Width, Height);
         Width = 1000;
         Height = 700;
+        undoQueue = new ArrayDeque<>();
         //Width = getLayoutParams().width;
-        currentBitmap = Bitmap.createBitmap(Width, Height, Bitmap.Config.ARGB_8888);
-        currentCanvas = new Canvas(currentBitmap);
+        //currentBitmap = Bitmap.createBitmap(Width, Height, Bitmap.Config.ARGB_8888);
+        //bufferBitmap = Bitmap.createBitmap(Width, Height, Bitmap.Config.ARGB_8888);
+        //currentCanvas = new Canvas(currentBitmap);
     }
 
     public myView(Context context)
@@ -105,18 +144,50 @@ class myView extends View
         //canvas.drawARGB(100, 30, 100, 10);
         Log.d("Debug", "" + getDrawingTime());
 
+        deBuffer();
         invalidate();
+    }
+
+    void deBuffer()
+    {
+        undoQueue.addFirst(Bitmap.createBitmap(currentBitmap));
+        if(undoQueue.size() > MAX_UNDO)
+            undoQueue.removeLast();
+        Canvas cv = new Canvas(currentBitmap);
+        cv.drawBitmap(bufferBitmap, 0, 0, null);
+        clearCanvas(currentCanvas);
+    }
+
+    public void undo()
+    {
+        if(undoQueue.isEmpty()) return;
+        Canvas cv = new Canvas(currentBitmap);
+        clearCanvas(cv);
+        cv.drawBitmap(undoQueue.getFirst(), 0, 0, null);
+        undoQueue.removeFirst();
+        invalidate();
+    }
+
+    public void setMode(int m)
+    {
+        mode = m;
+    }
+
+    void clearCanvas(Canvas cv)
+    {
+        cv.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh)
     {
-        /*if(!resized)
+        if(!resized)
         {
             currentBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            currentCanvas = new Canvas(currentBitmap);
+            bufferBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            currentCanvas = new Canvas(bufferBitmap);
             resized = true;
-        }*/
+        }
     }
 
     @Override
@@ -124,21 +195,68 @@ class myView extends View
     {
         super.onDraw(canvas);
         canvas.drawBitmap(currentBitmap, 0, 0, null);
+        canvas.drawBitmap(bufferBitmap, 0, 0, null);
         //canvas.setBitmap(currentBitmap);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
-        Paint pt = new Paint();
-        pt.setARGB(100, 100, 200, 10);
-        //currentCanvas.drawPoint(event.getX(), event.getY(), pt);
+        float x = event.getX();
+        float y = event.getY();
+        int act = event.getAction();
 
         Paint paint = new Paint();
         paint.setColor(Color.parseColor("#CD5C5C"));
-        currentCanvas.drawCircle(event.getX(), event.getY(), 10, paint);
+        paint.setStrokeWidth(10);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStyle(Paint.Style.STROKE);
 
-        Log.d("Debug", event.getX() + " " + event.toString());
+        if(mode == MODE_PENCIL)
+        {
+            if(act == MotionEvent.ACTION_DOWN)
+            {
+                pencilPath = new Path();
+                pencilPath.setLastPoint(x, y);
+            }
+            else if(act == MotionEvent.ACTION_MOVE)
+            {
+                float mx = (x + lastX) / 2;
+                float my = (y + lastY) / 2;
+                pencilPath.quadTo(lastX, lastY, mx, my);
+                currentCanvas.drawPath(pencilPath, paint);
+            }
+            else if(act == MotionEvent.ACTION_UP)
+            {
+                pencilPath.lineTo(x+0.1f, y+0.1f);
+                currentCanvas.drawPath(pencilPath, paint);
+                pencilPath = null;
+                deBuffer();
+            }
+
+            lastX = x;
+            lastY = y;
+        }
+        else if(mode == MODE_LINE)
+        {
+            if(act == MotionEvent.ACTION_DOWN)
+            {
+                lastX = x;
+                lastY = y;
+            }
+            else if(act == MotionEvent.ACTION_MOVE)
+            {
+                clearCanvas(currentCanvas);
+                currentCanvas.drawLine(lastX, lastY, x, y, paint);
+            }
+            else if(act == MotionEvent.ACTION_UP)
+            {
+                deBuffer();
+            }
+        }
+
+        Log.d("Debug", event.toString());
 
         invalidate();
 
